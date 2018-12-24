eccf522699d81a01bfb4f265f0590ed0bbc0653f
chams <alchemist107@gmail.com>
2018-12-24 16:43:10 +0100

    userInput handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-import Keyboard from "./components/Keyboard"
+import React, { Component } from "react";
+import "./App.css";
+import Keyboard from "./components/Keyboard";
+
+const controlTheUserInput = value => {
+  return value.substr(0, 16).replace(/\D/gi, "");
+};
 
 class App extends Component {
+  state = {
+    userInput: ""
+  };
+
+  handleChange(value) {
+    this.setState({
+      userInput: value
+    });
+  }
+
   render() {
     return (
-     <div className="Keyboard">
-         <h1>KeyBoard</h1>
-         <Keyboard/>
+      <div className="Keyboard">
+        <div>
+          <h2>Affichage</h2>
+          <h1>{controlTheUserInput(this.state.userInput)}</h1>
+        </div>
+        <div>
+          <h2>UserInput </h2>
+          <input
+            onChange={e => this.handleChange(e.target.value)}
+            value={controlTheUserInput(this.state.userInput)}
+          />
+        </div>
       </div>
     );
   }
